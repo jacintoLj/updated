cc05b647e2525b8adc7e0b6bac1e63ec3899db7d
mewtwo <151595728+jacintoLj@users.noreply.github.com>
2024-05-22 03:04:57 +0800

    Add files via upload

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Pet_Grooming
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.datecontainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(21, 535);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 65;
+            this.button4.Text = "Log Out";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(21, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 49);
+            this.button3.TabIndex = 64;
+            this.button3.Text = "History";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(21, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 49);
+            this.button2.TabIndex = 63;
+            this.button2.Text = "Cat Gromming";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 49);
+            this.button1.TabIndex = 62;
+            this.button1.Text = "Dog Gromming";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(746, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 21);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Saturday";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(655, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 21);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Friday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(545, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 21);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Thursday";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(437, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Wednesday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(348, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 21);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Tuesday";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 21);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Monday";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 21);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Sunday";
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPrevious.Location = new System.Drawing.Point(142, 117);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(68, 21);
+            this.buttonPrevious.TabIndex = 67;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNext.Location = new System.Drawing.Point(210, 117);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(68, 21);
+            this.buttonNext.TabIndex = 68;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // datecontainer
+            // 
+            this.datecontainer.Location = new System.Drawing.Point(132, 199);
+            this.datecontainer.Name = "datecontainer";
+            this.datecontainer.Size = new System.Drawing.Size(702, 387);
+            this.datecontainer.TabIndex = 69;
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMonth.Location = new System.Drawing.Point(144, 94);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(66, 20);
+            this.labelMonth.TabIndex = 70;
+            this.labelMonth.Text = "MONTH";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(836, 583);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.datecontainer);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Dashboard";
+            this.Text = "dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.FlowLayoutPanel datecontainer;
+        private System.Windows.Forms.Label labelMonth;
+    }
+}
---
+++ Dashboard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pet_Grooming
+{
+    public partial class Dashboard : Form
+    {
+        int month, year;
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            displayDays();
+        }
+        private void displayDays() 
+        {
+            DateTime now = DateTime.Now; 
+            month = now.Month;
+            year = now.Year;
+
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            labelMonth.Text = monthname + " " + year;
+
+            DateTime startofMonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int weekdays = Convert.ToInt32(startofMonth.DayOfWeek.ToString("d"));
+            
+            for (int i = 0; i < weekdays; i++) 
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                datecontainer.Controls.Add(ucblank);
+            } 
+            for (int i = 1;  i <= days; i++) 
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                datecontainer.Controls.Add(ucdays);
+            }
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            datecontainer.Controls.Clear();
+            month--;
+
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            labelMonth.Text = monthname + " " + year;
+
+            DateTime startofMonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int weekdays = Convert.ToInt32(startofMonth.DayOfWeek.ToString("d"));
+
+            for (int i = 0; i < weekdays; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                datecontainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                datecontainer.Controls.Add(ucdays);
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            datecontainer.Controls.Clear();
+            month++;
+
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            labelMonth.Text = monthname + " " + year;
+
+            DateTime startofMonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int weekdays = Convert.ToInt32(startofMonth.DayOfWeek.ToString("d"));
+
+            for (int i = 0; i < weekdays; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                datecontainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDays ucdays = new UserControlDays();
+                ucdays.days(i);
+                datecontainer.Controls.Add(ucdays);
+            }
+        }
+    }
+}
