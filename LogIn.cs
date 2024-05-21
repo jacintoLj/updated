9dad959920bb5018c726be3c09c6a5c9f66f178e
mewtwo <151595728+jacintoLj@users.noreply.github.com>
2024-05-22 03:03:53 +0800

    Add files via upload

---
+++ LogIn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pet_Grooming
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button1_Login_Click(object sender, EventArgs e)
+        {
+            if (textBox1_username.Text == "admin" && textBox2_Password.Text == "admin0220")
+            {
+                new Dashboard().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username or password.", "MessageBox", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+    }
+}
---
+++ UserControlDays.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Pet_Grooming
+{
+    partial class UserControlDays
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDays = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelDays
+            // 
+            this.labelDays.AutoSize = true;
+            this.labelDays.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDays.Location = new System.Drawing.Point(71, 4);
+            this.labelDays.Name = "labelDays";
+            this.labelDays.Size = new System.Drawing.Size(22, 17);
+            this.labelDays.TabIndex = 0;
+            this.labelDays.Text = "00";
+            // 
+            // UserControlDays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelDays);
+            this.Name = "UserControlDays";
+            this.Size = new System.Drawing.Size(94, 57);
+            this.Load += new System.EventHandler(this.UserControlDays_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDays;
+    }
+}
